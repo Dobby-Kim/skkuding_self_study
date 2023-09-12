d6fc676fdd1bbcc2b684a5ef8991460b31dd6330
Dobby_Kim <kimdobby@g.skku.edu>
2023-09-12 17:49:12 +0000

    승진의 가르침

--- week1/server.js
+++ week1/server.js
@@ -1,12 +1,32 @@
-const http = require("http");
-const fs = require("fs");
-const os = require("os");
+import express from "express";
+import os from "os";
+import fs from "fs";
+import router from "./dobby/index.js";
 
-const port = 3000;
+const PORT = 3000;
 
-http
-  .createServer((req, res) => {
-    fs.readFile("./week1.html", "utf8", (err, data) => {
+const app = express();
+
+app.get("/", async (req, res) => {
+  const data = await fs.promises.readFile("./week1.html", "utf8");
+  //! OS 정보를 HTML의 placeholder 자리에 넣기
+  const content = data
+    .replace("{{type}}", os.type())
+    .replace("{{hostname}}", os.hostname())
+    .replace("{{cpu_num}}", os.cpus().length)
+    .replace(`{{total_mem}}`, `${(os.totalmem() / 1024 ** 3).toFixed(2)} GB`);
+
+  //& 200 OK 상태 지정 후 유형이 html임을 지정
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(content); //& 클라이언트에 수정된 html 보냄*/
+});
+
+app.listen(PORT, () => {
+  console.log(`server is listening at localhost:${PORT}`);
+});
+
+/*http.createServer((req, res) => {
+     fs.readFile("./week1.html", "utf8", (err, data) => {
       if (err) {
         res.writeHead(500, { "Contents-Type": "text/plain" });
         res.end("Internal Server Error");
@@ -19,8 +39,8 @@ http
         .replace("{{hostname}}", os.hostname())
         .replace("{{cpu_num}}", os.cpus().length)
         .replace(
-          "{{total_name}}",
-          "${(os.totalmem() / (1024 ** 3)).toFixed(2)} GB"
+          `{{total_name}}`,
+          `${(os.totalmem() / (1024 ** 3)).toFixed(2)} GB`
         );
 
       //& 200 OK 상태 지정 후 유형이 html임을 지정
@@ -28,6 +48,6 @@ http
       res.end(content); //& 클라이언트에 수정된 html 보냄
     });
   })
-  .listen(port, "0.0.0.0", () => {
-    console.log("Server running at http://localhost:${port}/");
-  });
+  .listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+  });*/
